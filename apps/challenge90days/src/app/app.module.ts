c0594a42d1f6a49fce31991e9ea264ba1a2397b8
jason <jason.cheng@lileesystems.com>
2021-04-01 11:26:56 +0800

    add ng zorro

--- apps/challenge90days/src/app/app.module.ts
+++ apps/challenge90days/src/app/app.module.ts
@@ -3,11 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_TW } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTimelineModule } from 'ng-zorro-antd/timeline';
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  imports: [BrowserModule, HttpClientModule, FormsModule, BrowserAnimationsModule,NzButtonModule,NzTimelineModule],
+  providers: [{ provide: NZ_I18N, useValue: zh_TW }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
