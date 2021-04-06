cb51bf8ffea03539a9019a50137c84979daf3e94
jason zheng <tso1158687@gmail.com>
2021-04-07 00:51:36 +0800

    add slack api

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpService } from '@nestjs/common';
 
 import { Message } from '@challenge90days/api-interfaces';
 
@@ -6,10 +6,18 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,private httpService:HttpService) {}
 
   @Get('hello')
   getData(): Message {
     return this.appService.getData();
   }
+
+  @Get('checlin')
+  sendToSlack(){
+    const data={"text": "從ＡＰＩ來的訊息，大家安安"}
+
+    return this.httpService.post('https://hooks.slack.com/services/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',data)
+  }
+
 }
