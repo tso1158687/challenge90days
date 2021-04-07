aaddeef2f42b4db04caad0fc626908e1ed581834
jason <jason.cheng@lileesystems.com>
2021-04-07 10:10:43 +0800

    modify slack api content

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -6,18 +6,27 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService,private httpService:HttpService) {}
+  constructor(
+    private readonly appService: AppService,
+    private httpService: HttpService
+  ) {}
 
   @Get('hello')
   getData(): Message {
     return this.appService.getData();
   }
 
-  @Get('checlin')
-  sendToSlack(){
-    const data={"text": "從ＡＰＩ來的訊息，大家安安"}
+  @Get('checkin')
+  sendToSlack() {
+    const data = { text: '從API來的訊息，大家安安' };
+    console.log('ohohoh')
 
-    return this.httpService.post('https://hooks.slack.com/services/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',data)
+    this.httpService.post(
+      'https://hooks.slack.com/services/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',
+      data
+    ).subscribe(e=>{
+      console.log(e)
+      return  { message: 'ok' }
+    });
   }
-
 }
