d47f0609713d26dad32cf80044cc2400fba23cb8
jason zheng <tso1158687@gmail.com>
2021-04-10 22:45:47 +0800

    design landing page

--- apps/challenge90days/src/app/app.component.ts
+++ apps/challenge90days/src/app/app.component.ts
@@ -3,15 +3,30 @@ import { HttpClient } from '@angular/common/http';
 import { Message } from '@challenge90days/api-interfaces';
 import * as AOS from 'aos';
 import { footerData } from './data/footer';
+import { NbAuthJWTToken, NbAuthService } from '@nebular/auth';
+
 @Component({
   selector: 'challenge90days-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  user:any = {};
   footerData = footerData;
+  constructor(private authService: NbAuthService,private http: HttpClient){
+    this.authService.onTokenChange()
+      .subscribe((token: NbAuthJWTToken) => {
+
+        if (token.isValid()) {
+          this.user = token.getPayload(); // here we receive a payload from the token and assigns it to our `user` variable 
+          console.log(this.user)
+        }
+
+      });
+  }
   ngOnInit() {
     AOS.init();
+    // this.authService.
   }
 
   ngAfterViewInit(): void {
@@ -19,5 +34,4 @@ export class AppComponent implements OnInit {
     //Add 'implements AfterViewInit' to the class.
   }
   hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
 }
