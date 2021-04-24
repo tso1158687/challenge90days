488401426a62d9724dd8f6fb602ad77849608e67
jason zheng <tso1158687@gmail.com>
2021-04-24 22:19:37 +0800

    enable CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,6 +11,9 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  // TODO:要修改
+  // {origin:'https://challenage90days.web.app/'}
+  app.enableCors()
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
--- apps/challenge90days/src/app/services/checkin.service.ts
+++ apps/challenge90days/src/app/services/checkin.service.ts
@@ -6,7 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class CheckinService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.http.get("https://challenge-90-days.herokuapp.com/api/hello").subscribe(e=>{
+      console.log(e)
+    })
+  }
 
   addCheckin(message: string): Observable<any> {
     return this.http.post('/api/checkin', { message });
