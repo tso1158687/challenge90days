1ff1372b0905a38a5eb7ea0a4e739771d7dd40e6
jason zheng <tso1158687@gmail.com>
2021-04-25 11:38:13 +0800

    add firework library

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/challenge90days', '<rootDir>/apps/api'],
+  projects: [
+    '<rootDir>/apps/challenge90days',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/firework',
+  ],
 };
