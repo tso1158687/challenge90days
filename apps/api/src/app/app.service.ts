10bf06f9f8dbc2169f1a5b0d814dc68f01931f6a
jason zheng <tso1158687@gmail.com>
2021-04-25 16:40:30 +0800

    test cronjob

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -11,14 +11,20 @@ export class AppService {
   // @Cron('10 * * * * *')
   @Interval(1000000)
   testCronJob() {
-    let message = 'cron jon wroks';
-    console.log(message);
-    return { message };
+    const data = { text: '晚上11點囉，請大家記得打卡！😊' };
+    this.httpService.post(
+      process.env.slackApi,
+      data
+    ).subscribe(()=>{
+      return  { message: 'ok' }
+    },error=>{
+      return {message:'ohnono'}
+    });
   }
 
-  @Cron('0 0 * * * ')
+  @Cron('0 0 * * * *')
   notificationCheckin(){
-    const data = { text: '晚上11點囉，請大家記得打卡！😊' };
+    const data = { text: '晚上11點30分囉，請大家記得打卡！😊' };
     this.httpService.post(
       process.env.slackApi,
       data
