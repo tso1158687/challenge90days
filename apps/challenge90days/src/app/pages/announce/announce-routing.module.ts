d7f9bee1fb314aab1f36dd42bef65766cdd73b25
jason zheng <tso1158687@gmail.com>
2021-05-08 22:59:32 +0800

    add announce component and content

--- apps/challenge90days/src/app/pages/announce/announce-routing.module.ts
+++ apps/challenge90days/src/app/pages/announce/announce-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnnounceComponent } from './components/announce/announce.component';
 
 
 export const routes: Routes = [
-  // {
-  //   path: '',
-  //   component: LandingComponent,
-  // },
+  {
+    path: '',
+    component: AnnounceComponent,
+  },
 ];
 
 @NgModule({
--- apps/challenge90days/src/app/pages/announce/announce.module.ts
+++ apps/challenge90days/src/app/pages/announce/announce.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnnounceComponent } from './components/announce/announce.component';
 import { AnnounceRoutingModule } from './announce-routing.module';
-
-
+import { NbCardModule, NbIconModule } from '@nebular/theme';
 
 @NgModule({
-  declarations: [
-    AnnounceComponent
-  ],
-  imports: [
-    CommonModule,
-    AnnounceRoutingModule
-  ]
+  declarations: [AnnounceComponent],
+  imports: [CommonModule, AnnounceRoutingModule, NbIconModule,NbCardModule],
 })
-export class AnnounceModule { }
+export class AnnounceModule {}
