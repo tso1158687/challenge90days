c381c259b5041f1b58f493f3bedf09d6f26c537c
jason zheng <tso1158687@gmail.com>
2021-05-09 14:36:32 +0800

    add context menu

--- apps/challenge90days/src/app/app.component.ts
+++ apps/challenge90days/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@challenge90days/api-interfaces';
 import * as AOS from 'aos';
 import { footerData } from './data/footer';
 import { NbAuthJWTToken, NbAuthService } from '@nebular/auth';
 import { FireworkService } from './services/firework.service';
 import { BehaviorSubject } from 'rxjs';
+import { NbMenuItem, NbMenuService } from '@nebular/theme';
+import { filter, map, take } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'challenge90days-root',
@@ -16,18 +17,22 @@ export class AppComponent implements OnInit {
   showFirework$: BehaviorSubject<boolean>;
   user: any = {};
   footerData = footerData;
-  items = [
-    { title: '設定' },
-    { title: '成就' },
-    { title: '登出' },
-
+  settingList: NbMenuItem[] = [
+    { title: '設定', url: 'settings', icon: 'settings-outline' },
+    { title: '公告', url: 'announce',icon: 'message-circle-outline'  },
+    { title: '成就', url: 'checkin/challenge',icon: 'award-outline'  },
+    { title: '登出', url: 'logout',icon: 'message-circle-outline'  },
   ];
+
   constructor(
+    private router: Router,
     private authService: NbAuthService,
-    private http: HttpClient,
-    private fireworkService: FireworkService
+    private fireworkService: FireworkService,
+    private nbMenuService: NbMenuService
   ) {
     this.authService.onTokenChange().subscribe((token: NbAuthJWTToken) => {
+      console.log(token);
+      console.log(token.isValid());
       if (token.isValid()) {
         this.user = token.getPayload(); // here we receive a payload from the token and assigns it to our `user` variable
         console.log(this.user);
@@ -37,11 +42,32 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.showFirework$ = this.fireworkService.showFirework$;
     AOS.init();
-    // this.authService.
+    this.clickMenuItem();
   }
 
-  ngAfterViewInit(): void {
-    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-    //Add 'implements AfterViewInit' to the class.
+  clickMenuItem() {
+    console.log('click')
+    this.nbMenuService
+      .onItemClick()
+      .pipe(
+        filter(({ tag }) => tag === 'my-context-menu'),
+        map((e) => e.item)
+      )
+      .subscribe((title) => {
+        console.log(title.url)
+        if(title.url!=='logout'){
+          this.router.navigate([title.url]);
+        }else{
+          this.logout()
+        }
+      });
+  }
+
+  logout(){
+    console.log('logout')
+    this.authService.logout('password').pipe(take(1)).subscribe(e=>{
+      console.log(e)
+    })
+    // this.authService.refreshToken('email')
   }
 }
