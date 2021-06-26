e1a1f2ba5cf9c692dae046f14fdd5a3beda77646
jason zheng <tso1158687@gmail.com>
2021-06-26 12:04:05 +0800

    add pages moudle

--- apps/challenge90days/src/app/app-routing.module.ts
+++ apps/challenge90days/src/app/app-routing.module.ts
@@ -1,20 +1,8 @@
 import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  {
-    path: 'checkin',
-    canActivate: [AuthGuard],
-    loadChildren: () =>
-      import('./pages/checkin/checkin.module').then((m) => m.CheckinModule),
-  },
-  {
-    path: 'announce',
-    loadChildren: () =>
-      import('./pages/announce/announce.module').then((m) => m.AnnounceModule),
-  },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
@@ -22,7 +10,7 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
-      import('./pages/intro/intro.module').then((m) => m.IntroModule),
+      import('./pages/pages.module').then((m) => m.PagesModule),
   },
   {
     path: '**',
