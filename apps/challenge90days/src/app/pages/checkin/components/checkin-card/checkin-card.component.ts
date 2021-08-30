566e48fd6c1ac3b8bcfbe090b9bf8c8f3e52b058
jason zheng <tso1158687@gmail.com>
2021-08-30 15:41:10 +0800

    adjust slide card

--- apps/challenge90days/src/app/pages/checkin/components/checkin-card/checkin-card.component.ts
+++ apps/challenge90days/src/app/pages/checkin/components/checkin-card/checkin-card.component.ts
@@ -1,14 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+} from '@angular/core';
 import { Checkin } from '@challenge90days/api-interfaces';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'challenge90days-checkin-card',
   templateUrl: './checkin-card.component.html',
   styleUrls: ['./checkin-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CheckinCardComponent {
   @Input() isSelf = false;
   @Input() status = '';
   @Input() checkin: Checkin;
+  randomId = (Math.random() + 1).toString(36).substring(7);
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+  getRandomIdTag(): string {
+    const randomIdTag = `#${this.randomId}`;
+    this.changeDetectorRef.markForCheck();
+    return randomIdTag;
+  }
 }
